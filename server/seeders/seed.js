de83877fef245f702b7b26500d7400ee0fa39733
afrazier01 <amfrazier01@gmail.com>
2023-08-28 20:51:23 -0500

    successfully seeded and started server. Working on queries and mutations

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,12 +1,12 @@
 const db = require('../config/connection');
 const { Loan } = require('../models');
-const loanSeeds = require('./loanSeed');
+const loanSeed = require('./loanSeed.json');
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   await cleanDB('Loan', 'loans');
-
-  await Loan.create(loanSeeds);
+  
+  await Loan.create(loanSeed);
 
   console.log('all done!');
   process.exit(0);
