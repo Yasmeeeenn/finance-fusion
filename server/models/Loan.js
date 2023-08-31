9e357abf9591854c57aae4c9021b9bf4b3accd55
unknown <barronpeterson56@gmail.com>
2023-08-31 13:30:29 -0500

    add monthly payment logic

--- server/models/Loan.js
+++ server/models/Loan.js
@@ -37,11 +37,13 @@ const loanSchema = new Schema({
 loanSchema.pre('save', function (next) {
   const interestRateDecimal = this.interestRate / 100;
   const totalPayments = this.loanTerm * 12;
+
   this.totalInterest = parseFloat(((this.loanPrinciple * interestRateDecimal * totalPayments) / 12).toFixed(2));
   this.totalLoanAmount = parseFloat((this.totalInterest + this.loanPrinciple).toFixed(2));
 
-  console.log('Calculated totalInterest:', this.totalInterest);
-  console.log('Calculated totalLoanAmount:', this.totalLoanAmount);
+  const monthlyInterestRate = interestRateDecimal / 12;
+  this.monthlyPayment = (this.totalLoanAmount * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, totalPayments)) / (Math.pow(1 + monthlyInterestRate, totalPayments) - 1);
+  this.monthlyPayment = parseFloat(this.monthlyPayment.toFixed(2));
 
   next();
 });
