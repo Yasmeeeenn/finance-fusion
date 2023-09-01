5920dd644a05bfd779e4f5b5f0143b62323bb0f9
afrazier01 <amfrazier01@gmail.com>
2023-08-31 20:39:30 -0500

    reorganizing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,35 +1,27 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App.jsx'
-import Home from './pages/Home'
-import LoginPage from './pages/LoginPage.jsx'
-import Error from './pages/Error'
-import SignupPage from './pages/SignupPage.jsx'
-
-import './font.css';
+import SearchBooks from './pages/SearchBooks'
+import SavedBooks from './pages/SavedBooks'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <Error />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
     children: [
       {
         index: true,
-        element: <Home />,
-      },
-
-      {
-        path: '/login',
-        element: <LoginPage />,
+        element: <SearchBooks />
       },
       {
-        path: '/signup',
-        element: <SignupPage />,
-      },
-    ],
-  },
+        path: '/saved',
+        element: <SavedBooks />
+      }
+    ]
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
