e0d6bc53119c604cefdd63c69ad52b09d25fbf04
afrazier01 <amfrazier01@gmail.com>
2023-08-31 21:05:18 -0500

    modifications to main and app jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App.jsx'
-import SearchBooks from './pages/SearchBooks'
-import SavedBooks from './pages/SavedBooks'
+import Home from './pages/Home.jsx'
+import SavedLoans from './pages/SavedLoans.jsx'
 
 const router = createBrowserRouter([
   {
@@ -14,11 +13,11 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <SearchBooks />
+        element: <Home />
       },
       {
         path: '/saved',
-        element: <SavedBooks />
+        element: <SavedLoans />
       }
     ]
   }
