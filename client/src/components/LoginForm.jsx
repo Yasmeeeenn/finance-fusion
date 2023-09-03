3113e04f0075bf940ee881db943791c0630c8b97
afrazier01 <amfrazier01@gmail.com>
2023-09-02 20:23:56 -0500

    login functionality added

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -26,7 +26,7 @@ const LoginForm = () => {
     
     try {
       
-      const {data}  = await loginUser({
+      const {data}  = await login({
         variables: { email, password} ,
       });
 
